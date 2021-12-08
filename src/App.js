f2cb7392a1b6fc6c7bbe32da20953d6e6e2e201f
Nguyen Cong Duy <ncongduy@gmail.com>
2021-12-08 14:51:07 +0200

    feat: react dom - create routing for each feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
+import { Route } from 'react-router';
 
 function App() {
 	return (
 		<div>
-			<TodoFeature />
-			{/* <AlbumFeature /> */}
+			<h2>Header</h2>
+			<Route path='/todos' component={TodoFeature} />
+			<Route path='/albums' component={AlbumFeature} />
+			<h2>Footer</h2>
 		</div>
 	);
 }
