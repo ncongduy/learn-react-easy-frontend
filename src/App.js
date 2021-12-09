1240d36a77f7ca5411b13dfc7f871d5c8f1209e0
Nguyen Cong Duy <ncongduy@gmail.com>
2021-12-09 07:00:45 +0200

    feat: use Link and NavLink - React Router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import './App.css';
 import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 import { Route } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/albums">Albums</Link>
+      </p>
+
+      <p>
+        <NavLink to="/todos">Todos</NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums">Albums</NavLink>
+      </p>
+
       <h2>Header</h2>
       <Route path="/todos" component={TodoFeature} />
       <Route path="/albums" component={AlbumFeature} />
